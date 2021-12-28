97b914b5437d0a3eb9c220759d28fe3e0368e71d
Pyrix25633 <biral.mattia@gmail.com>
2021-12-28 10:42:08 +0100

    1.18.1-1.0.1 (probably last version)

--- src/main/java/net/pyrix25633/wild_update/block/ModBlocks.java
+++ src/main/java/net/pyrix25633/wild_update/block/ModBlocks.java
@@ -24,138 +24,167 @@ public class ModBlocks {
             new MudBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(0.85f, 1f)
                     .jumpVelocityMultiplier(0.9f)
-                    .sounds(BlockSoundGroup.ROOTED_DIRT)));
+                    .sounds(BlockSoundGroup.ROOTED_DIRT)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MUD_BRICKS = registerBlock("mud_bricks",
             new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(mudBrickHardness, mudBrickResistance)
                     .requiresTool()
-                    .sounds(BlockSoundGroup.STONE)));
+                    .sounds(BlockSoundGroup.STONE)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MUD_BRICK_STAIRS = registerBlock("mud_brick_stairs",
             new ModStairsBlock(ModBlocks.MUD_BRICKS.getDefaultState(),
                     FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(mudBrickHardness, mudBrickResistance)
                     .requiresTool()
-                    .sounds(BlockSoundGroup.STONE)));
+                    .sounds(BlockSoundGroup.STONE)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MUD_BRICK_SLAB = registerBlock("mud_brick_slab",
             new SlabBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(mudBrickHardness, mudBrickResistance)
                     .requiresTool()
-                    .sounds(BlockSoundGroup.STONE)));
+                    .sounds(BlockSoundGroup.STONE)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MUD_BRICK_WALL = registerBlock("mud_brick_wall",
             new WallBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(mudBrickHardness, mudBrickResistance)
                     .requiresTool()
-                    .sounds(BlockSoundGroup.STONE)));
+                    .sounds(BlockSoundGroup.STONE)),
+            ItemGroup.BUILDING_BLOCKS);
     //Mangrove Blocks
     public static final Block MANGROVE_LOG = registerBlock("mangrove_log",
             new ModLogBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(1f, 1f)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block STRIPPED_MANGROVE_LOG = registerBlock("stripped_mangrove_log",
             new ModLogBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(1f, 1f)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_WOOD = registerBlock("mangrove_wood",
             new ModLogBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(1f, 1f)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block STRIPPED_MANGROVE_WOOD = registerBlock("stripped_mangrove_wood",
             new ModLogBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(1f, 1f)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_PLANKS = registerBlock("mangrove_planks",
             new Block(FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_STAIRS = registerBlock("mangrove_stairs",
             new ModStairsBlock(ModBlocks.MUD_BRICKS.getDefaultState(),
                     FabricBlockSettings.of(Material.WOOD)
                             .strength(mangroveHardness, mangroveResistance)
-                            .sounds(BlockSoundGroup.WOOD)));
+                            .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_SLAB = registerBlock("mangrove_slab",
             new SlabBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_FENCE = registerBlock("mangrove_fence",
             new FenceBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_FENCE_GATE = registerBlock("mangrove_fence_gate",
             new FenceGateBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_BUTTON = registerBlock("mangrove_button",
             new ModWoodenButtonBlock(FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_PRESSURE_PLATE = registerBlock("mangrove_pressure_plate",
             new ModPressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING,
                     FabricBlockSettings.of(Material.WOOD)
                     .strength(mangroveHardness, mangroveResistance)
-                    .sounds(BlockSoundGroup.WOOD)));
+                    .sounds(BlockSoundGroup.WOOD)),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_DOOR = registerBlock("mangrove_door",
             new ModDoorBlock(FabricBlockSettings.of(Material.WOOD)
                             .strength(mangroveHardness, mangroveResistance)
-                            .sounds(BlockSoundGroup.WOOD).nonOpaque()));
+                            .sounds(BlockSoundGroup.WOOD).nonOpaque()),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_TRAPDOOR = registerBlock("mangrove_trapdoor",
             new ModTrapdoorBlock(FabricBlockSettings.of(Material.WOOD)
                             .strength(mangroveHardness, mangroveResistance)
-                            .sounds(BlockSoundGroup.WOOD).nonOpaque()));
+                            .sounds(BlockSoundGroup.WOOD).nonOpaque()),
+            ItemGroup.BUILDING_BLOCKS);
     public static final Block MANGROVE_LEAVES = registerBlock("mangrove_leaves",
             new LeavesBlock(FabricBlockSettings.of(Material.LEAVES)
                     .strength(0.2f, 0.2f)
-                    .sounds(BlockSoundGroup.AZALEA_LEAVES).nonOpaque()));
+                    .sounds(BlockSoundGroup.AZALEA_LEAVES).nonOpaque()),
+            ItemGroup.DECORATIONS);
     public static final Block MANGROVE_ROOTS = registerBlock("mangrove_roots",
             new MangroveRoots(FabricBlockSettings.of(Material.SOLID_ORGANIC)
                     .strength(0.4f, 0.4f)
-                    .sounds(BlockSoundGroup.ROOTS).nonOpaque()));
+                    .sounds(BlockSoundGroup.ROOTS).nonOpaque()),
+            ItemGroup.DECORATIONS);
     public static final Block MANGROVE_PROPAGULE = registerBlock("mangrove_propagule",
             new MangrovePropagule(FabricBlockSettings.copy(Blocks.OAK_SAPLING)
-                    .nonOpaque().sounds(BlockSoundGroup.GRASS)));
+                    .nonOpaque().sounds(BlockSoundGroup.GRASS)),
+            ItemGroup.DECORATIONS);
     //Sculk blocks
     public static final Block SCULK_BLOCK = registerBlock("sculk_block",
             new SculkBlock(FabricBlockSettings.of(Material.SCULK)
                     .strength(0.5f, 1.2f)
-                    .sounds(BlockSoundGroup.SCULK_SENSOR)));
+                    .sounds(BlockSoundGroup.SCULK_SENSOR)),
+            ItemGroup.DECORATIONS);
     public static final Block SCULK_CATALYST = registerBlock("sculk_catalyst",
             new CatalystBlock(FabricBlockSettings.of(Material.SCULK)
                     .strength(1f, 1.2f)
-                    .sounds(BlockSoundGroup.SCULK_SENSOR)));
+                    .sounds(BlockSoundGroup.SCULK_SENSOR)),
+            ItemGroup.DECORATIONS);
     public static final Block SCULK_SHRIEKER = registerBlock("sculk_shrieker",
             new ShriekerBlock(FabricBlockSettings.of(Material.SCULK)
                     .strength(1f, 1.2f)
-                    .sounds(BlockSoundGroup.SCULK_SENSOR).nonOpaque()));
+                    .sounds(BlockSoundGroup.SCULK_SENSOR).nonOpaque()),
+            ItemGroup.DECORATIONS);
     public static final Block SCULK_VEIN = registerBlock("sculk_vein",
             new VeinBlock(FabricBlockSettings.of(Material.SCULK)
                     .strength(0.5f, 1.2f)
-                    .sounds(BlockSoundGroup.SCULK_SENSOR).nonOpaque()));
+                    .sounds(BlockSoundGroup.SCULK_SENSOR).nonOpaque()),
+            ItemGroup.DECORATIONS);
     //Hollow Birch Log
     public static final Block HOLLOW_BIRCH_LOG = registerBlock("hollow_birch_log",
             new HollowLog(FabricBlockSettings.of(Material.WOOD)
                     .strength(hollowHardness, hollowResistance)
-                    .sounds(BlockSoundGroup.WOOD).nonOpaque()));
+                    .sounds(BlockSoundGroup.WOOD).nonOpaque()),
+            ItemGroup.DECORATIONS);
     public static final Block STRIPPED_HOLLOW_BIRCH_LOG = registerBlock("stripped_hollow_birch_log",
             new HollowLog(FabricBlockSettings.of(Material.WOOD)
                     .strength(hollowHardness, hollowResistance)
-                    .sounds(BlockSoundGroup.WOOD).nonOpaque()));
+                    .sounds(BlockSoundGroup.WOOD).nonOpaque()),
+            ItemGroup.DECORATIONS);
     //Flowers
     public static final Block DICHONDRA = registerBlock("dichondra",
             new ShortFlowerBlock(StatusEffects.HEALTH_BOOST,200,
                     FabricBlockSettings.copy(Blocks.DANDELION)
                     .breakInstantly()
-                    .sounds(BlockSoundGroup.CROP).nonOpaque().noCollision()));
+                    .sounds(BlockSoundGroup.CROP).nonOpaque().noCollision()),
+            ItemGroup.DECORATIONS);
     public static final Block POTTED_DICHONDRA = registerOnlyBlock("potted_dichondra",
             new FlowerPotBlock(ModBlocks.DICHONDRA, FabricBlockSettings.copy(Blocks.POTTED_DANDELION)));
     public static final Block NEMESIA_KLM = registerBlock("nemesia_klm",
             new ShortFlowerBlock(StatusEffects.GLOWING,200,
                     FabricBlockSettings.copy(Blocks.DANDELION)
                             .breakInstantly()
-                            .sounds(BlockSoundGroup.CROP).nonOpaque().noCollision()));
+                            .sounds(BlockSoundGroup.CROP).nonOpaque().noCollision()),
+            ItemGroup.DECORATIONS);
     public static final Block POTTED_NEMESIA_KLM = registerOnlyBlock("potted_nemesia_klm",
             new FlowerPotBlock(ModBlocks.NEMESIA_KLM, FabricBlockSettings.copy(Blocks.POTTED_DANDELION)));
 
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
+    private static Block registerBlock(String name, Block block, ItemGroup itemGroup) {
+        registerBlockItem(name, block, itemGroup);
         return Registry.register(Registry.BLOCK, new Identifier(WildUpdate.MOD_ID, name), block);
     }
 
@@ -163,9 +192,9 @@ public class ModBlocks {
         return Registry.register(Registry.BLOCK, new Identifier(WildUpdate.MOD_ID, name), block);
     }
 
-    private static Item registerBlockItem(String name, Block block) {
+    private static Item registerBlockItem(String name, Block block, ItemGroup itemGroup) {
         return Registry.register(Registry.ITEM, new Identifier(WildUpdate.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+                new BlockItem(block, new FabricItemSettings().group(itemGroup)));
     }
 
     public static void registerModBlocks() {
